154ddd4dd75c700295e96acb424a5baaf7cb50c7
Shresthi Kashyap <sk2929542@gmail.com>
2024-10-10 15:47:24 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,42 @@
-const express = require('express');
+const express = require('express'); 
 const mongoose = require('mongoose');
-const dotenv = require('dotenv')
-const stockController = require('./controllers/stockController')
+const dotenv = require('dotenv');
+const stockController = require('./controllers/stockController');
 const cors = require('cors');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path'); // Import path module
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_URL : 'http://localhost:3000',// Allow the frontend origin
-        methods: ["GET", "POST"],          // Specify allowed methods
-        allowedHeaders: ["Content-Type"],   // Specify allowed headers
-        credentials: true                   // Allow credentials if needed
+        origin: process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_URL : 'http://localhost:3000',
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type"],
+        credentials: true
     }
 });
 
-dotenv.config();
-
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
+// Serve static files in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => res.send('Server not set to production'));
+}
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Socket.IO connection
 io.on('connection', (socket) => {
@@ -38,4 +53,3 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
